d9d0a4e91e9a56a921946af2889d52bdf4fbef80
Thiago Loureiro <thiagoguaru@outlook.com>
2017-11-14 00:32:25 +0100

    Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            log.Error("Hello World");
+            log.Warn("Hello World");
+            log.Info("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
